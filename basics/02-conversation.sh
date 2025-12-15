1e34848a4fc7dcdebbdd1aa8bceeba4653416646
sujeevgarlapati <125922127+sujeevgarlapati@users.noreply.github.com>
2025-12-15 14:38:44 +0000

    conversation

---
+++ basics/02-conversation.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+echo "sujeev :: Hello Mahesh, How are you?"
+echo "mahesh :: Hi Sujeev, I am fine. How about you?"
+echo "sujeev :: I am also good. What are you doing these days?"
+echo "mahesh :: Just learning some shell scripting."
