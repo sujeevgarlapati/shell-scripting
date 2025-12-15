bb5df49a50ee0f08454c472e6d7b67bd03e30dc6
sujeevgarlapati <125922127+sujeevgarlapati@users.noreply.github.com>
2025-12-15 14:52:23 +0000

    variables

---
+++ basics/03-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+#no space between variable name and equal sign
+echo person1=Nani
+echo person2=Babu
+
+
+echo "$person1 :: Hello $person2, How are you?"
+echo "$person2 :: Hi $person1, I am fine. How about you?"
+echo "$person1 :: I am also good. What are you doing these days?"
+echo "$person2 :: Just learning some shell scripting."
