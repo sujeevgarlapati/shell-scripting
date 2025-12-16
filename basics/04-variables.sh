c70fc5f63c92fb74df0d4d34748431cd3ae861b7
sujeevgarlapati <125922127+sujeevgarlapati@users.noreply.github.com>
2025-12-16 09:06:46 +0000

    variables

---
+++ basics/04-variables.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+#Arguments
+person1=$1
+person2=$2
+echo "$person1 :: Hello $person2, How are you?"
+echo "$person2 :: Hi $person1, I am fine. How about you?"
+echo "$person1 :: I am also good. What are you doing these days?"
+echo "$person2 :: Just learning some shell scripting."
