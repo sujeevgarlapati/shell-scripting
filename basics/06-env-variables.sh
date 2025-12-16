d0aab263f10964692344273bbd0bbcdda2f4b778
sujeevgarlapati <125922127+sujeevgarlapati@users.noreply.github.com>
2025-12-16 10:49:20 +0000

    env variables

--- basics/06-env-variables.sh
+++ basics/06-env-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo"course_name:$COURSE_NAME"
-echo"student_name:$STUDENT_NAME"
-echo"student_id:$STUDENT_ID"
+echo "course_name:$COURSE_NAME"
+echo "student_name:$STUDENT_NAME"
+echo "student_id:$STUDENT_ID"
