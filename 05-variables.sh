52dc84b7509c9c440be38b7170ea61f33e13c6d4
sujeevgarlapati <125922127+sujeevgarlapati@users.noreply.github.com>
2025-12-16 10:00:28 +0000

    Read parameters in variables

---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter your pin number"
+read -s PIN_NUMBER 
+
+# -s flag makes the input silent (not displayed on screen) 
+# Here PIN_NUMBER is a variable that stores the user input
+echo "Your pin number is $PIN_NUMBER"
