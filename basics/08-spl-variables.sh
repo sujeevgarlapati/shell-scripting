beb100b61866f2cca0093e86231bab6eb24dbf23
sujeevgarlapati <125922127+sujeevgarlapati@users.noreply.github.com>
2025-12-17 07:54:42 +0000

    special_variables

--- basics/08-spl-variables.sh
+++ basics/08-spl-variables.sh
@@ -9,3 +9,6 @@ echo "Home directory of user: $HOME"
 echo "PID of this script: $$"
 sleep 50 &
 echo "PID of the last command in background is: $!"
+
+# Made the script executable by running: chmod +x 08-spl-variables.sh
+# Run the script by: ./08-spl-variables.sh arg1 arg2 arg3 or bash 08-spl-variables.sh arg1 arg2 arg3
